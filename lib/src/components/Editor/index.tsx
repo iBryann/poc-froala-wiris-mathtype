bf878ac3aee354fbfcf4f1a8f40f09a4d8fc636d
Bryann Henrique <bryann.brandao@sistemapoliedro.com.br>
2025-01-10 16:02:56 -0300

    update

--- lib/src/components/Editor/index.tsx
+++ lib/src/components/Editor/index.tsx
@@ -2,6 +2,7 @@
 import { useMemo } from 'react';
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/languages/pt_br.js';
 import 'froala-editor/js/plugins.pkgd.min.js';
 import FroalaEditorComponent from 'react-froala-wysiwyg';
 import { TEditor } from './types';
@@ -26,6 +27,7 @@ export const Editor = ({ value = '', licenseKey, onChangeValue = () => {} }: TEd
   const froalaConfig = useMemo(
     () => ({
       key: licenseKey,
+      language: 'pt_br',
       toolbarButtons: [
         'fontFamily',
         'fontSize',
